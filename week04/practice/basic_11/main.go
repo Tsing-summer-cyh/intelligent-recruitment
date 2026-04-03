63f2ff163837dbd40c46ed1fd27bf2d623f24a17
cyh <1463117317@qq.com>
2026-04-03 17:12:45 +0800

    feat: 完成 week04 基础练习 basic_11 变量定义与打印

---
+++ week04/practice/basic_11/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	// 1. 定义变量
+	var name string = "小明"
+	var age int = 23
+	var gender bool = true // 男为 true，女为 false
+
+	// 2. 逐行输出对应的信息
+	fmt.Println(name)
+	fmt.Println(age)
+	fmt.Println(gender)
+}
